aab7951c6fe27f7ce4b4fb02a43ec032398d3f54
noobzyl <20204507011@stu.suda.edu.cn>
2020-10-25 17:29:56 +0800

    修改

--- guess.py
+++ guess.py
@@ -8,16 +8,22 @@ while counts >=0:
     temp = input("请输入一个100以内的正整数： ")
     guess = int(temp)
 
-    if guess == answer:
-        counts = counts + 1
-        print("猜中啦！")
-        print("你猜了：" + str(counts) + "次")
+    if guess > 100:
+        print("超出范围啦")
         break
+        
     else:
-        if guess < answer:
-            print("小了")
+        if guess == answer:
+            counts = counts + 1
+            print("猜中啦！")
+            print("你猜了：" + str(counts) + "次")
+            break
         else:
-            print("大了")
-        counts = counts + 1
+            if guess < answer:
+                print("小了")
+                counts = counts + 1
+            else:
+                print("大了")
+                counts = counts + 1
 
 print("游戏结束")
